11a5a692dc4bceed4966211e1b95584bcf0889fa
Rodolfo <rodolfoidez@gmail.com>
2015-01-02 20:44:16 -0300

    mysql
    mysql

---
+++ docs/BD/trigger.sql
@@ -0,0 +1,30 @@
+DELIMITER $$
+
+CREATE TRIGGER check_delete 
+	after delete ON beep_evento_check for each row 
+BEGIN
+	DECLARE updatecount integer;
+  SET updatecount = ( SELECT count(*) FROM beep_evento_check  WHERE eve_id = old.eve_id);
+  IF updatecount>0
+    THEN
+      UPDATE beep_evento SET eve_check = updatecount WHERE eve_id = old.eve_id;
+  END IF;
+END$$
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE TRIGGER check_update 
+	after delete ON beep_evento_check for each row 
+BEGIN
+	DECLARE updatecount integer;
+  SET updatecount = ( SELECT count(*) FROM beep_evento_check  WHERE eve_id = new.eve_id);
+  IF updatecount>0
+    THEN
+      UPDATE beep_evento SET eve_check = updatecount WHERE eve_id = new.eve_id;
+  END IF;
+END$$
+
+DELIMITER ;
